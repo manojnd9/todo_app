b6bcbde8d3bea11c77216f60500cbb71a1bc5be1
manojnd9 <manojnd9@gmail.com>
2024-12-31 19:40:47 +0100

    set up and connect with psql

--- todo_app/database.py
+++ todo_app/database.py
@@ -2,12 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
-"""URL to create a location of this DataBase on the FastAPI application."""
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:test1234!@localhost/TodoApplicationDatabase"
+"""URL to create a location of this DataBase on the FastAPI application.
+    Give path where database is located.
+    For sqllite, if db is created locally within the application, the URL looks like this:
+    sqlite:///./todosapp.db
+"""
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 """Engine to connect the database session with the fastAPI application"""
 
 
