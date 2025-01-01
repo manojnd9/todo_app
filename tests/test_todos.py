e9e9f5e3e2dcb7d721238f78ae3dccd2a72539b0
manojnd9 <manojnd9@gmail.com>
2025-01-01 19:27:54 +0100

    util test

--- tests/test_todos.py
+++ tests/test_todos.py
@@ -1,73 +1,19 @@
-from sqlalchemy import create_engine, text
-from sqlalchemy.pool import StaticPool
-from sqlalchemy.orm import sessionmaker
-from fastapi.testclient import TestClient
 from starlette import status
-import pytest
 
-from todo_app.database import Base
-from todo_app.main import app
-from todo_app.routers.auth import get_current_user
-from todo_app.routers.todos import get_db
-from todo_app.models import ToDos
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///./testdb.db"
-"""Test database URL"""
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False},
-    poolclass=StaticPool,
+from tests.utils import (
+    override_get_current_user,
+    override_get_db,
+    client,
+    TestSessionLocal,
+    test_todo,
 )
-"""DB Engine to connect with test db"""
-
-TestSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-"""Test session sqlalchemy db session connected with db engine"""
-Base.metadata.create_all(bind=engine)
-
-
-def override_get_db():
-    """DB injection function to connect with local
-    test db session. This is used to override the prod db dependency
-    injecting function during testing,
-    """
-    db = TestSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-def override_get_current_user():
-    return {"username": "manojndtest", "id": 1, "user_role": "admin"}
+from todo_app.main import app
+from todo_app.routers.todos import get_db, get_current_user
+from todo_app.models import ToDos
 
 
 app.dependency_overrides[get_db] = override_get_db
 app.dependency_overrides[get_current_user] = override_get_current_user
-
-
-client = TestClient(app)
-
-
-# Todo test fixture
-
-
-@pytest.fixture(scope="function")
-def test_todo():
-    todo = ToDos(
-        title="Learn to code!",
-        description="Need to learn!",
-        priority=5,
-        complete=False,
-        owner_id=1,
-    )
-    db = TestSessionLocal()
-    db.add(todo)
-    db.commit()
-    yield db
-    with engine.connect() as connection:
-        connection.execute(text("DELETE FROM TODOS;"))
-        connection.commit()
 
 
 def test_read_all_authenticated(test_todo):
