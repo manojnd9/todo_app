aaf0e0cb6dcbf539435042a56686a117814a0054
manojnd9 <manojnd9@gmail.com>
2025-01-03 20:15:21 +0100

    create new todo functionality

--- todo_app/routers/todos.py
+++ todo_app/routers/todos.py
@@ -77,6 +77,20 @@ async def render_todo_page(request: Request, db: db_dependency):
         return redirect_to_login()
 
 
+@router.get("/add-todo-page")
+async def render_add_todo_page(request: Request):
+    try:
+        user = await get_current_user(request.cookies.get("access_token"))
+        if user is None:
+            return redirect_to_login()
+        return templates.TemplateResponse(
+            "add-todo.html", {"request": request, "user": user}
+        )
+    except Exception as e:
+        print(f"Error: {e}")
+        return redirect_to_login()
+
+
 ### End Points ###
 @router.get("/", status_code=status.HTTP_200_OK)
 async def read_all(user: user_dependency, db: db_dependency):
